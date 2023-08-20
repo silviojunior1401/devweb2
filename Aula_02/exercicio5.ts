30df01c844c79cd5c5f11e4440a9c17c44d26786
silviojunior1401 <silviojunior1401@gmail.com>
2023-08-19 23:46:15 -0300

    Ajustes Exercicio 5 Aula 2

--- Aula_02/exercicio5.ts
+++ Aula_02/exercicio5.ts
@@ -1,12 +1,10 @@
 namespace exercicio5 {
     let horaAtual: number = new Date().getHours();
 
-    //let horaAtual: number = 22;
-
     let nome: string = "Silvio";
 
-    let saudacao: string = horaAtual >= 18 ? "Bom noite" : 
-        horaAtual >= 12 ? "Boa tarde" : horaAtual >= 6 ? "Boa dia" : "Boa madrugada"
+    let saudacao: string = horaAtual >= 18 ? "Boa noite" : 
+        horaAtual >= 12 ? "Boa tarde" : horaAtual >= 6 ? "Bom dia" : "Boa madrugada"
 
     console.log(`${saudacao} ${nome}!`);
 }
